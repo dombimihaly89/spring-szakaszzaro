b7c1269201b1c8471af5f261966d251af7588ae3
Mihaly Dombi <dombimihaly89@gmail.com>
2020-01-28 17:10:06 +0100

    controllers are done

--- src/main/java/hu/flowacademy/musicstore/resources/SongController.java
+++ src/main/java/hu/flowacademy/musicstore/resources/SongController.java
@@ -1,5 +1,49 @@
 package hu.flowacademy.musicstore.resources;
 
+import hu.flowacademy.musicstore.models.Artist;
+import hu.flowacademy.musicstore.models.DTOs.ArtistDTO;
+import hu.flowacademy.musicstore.models.DTOs.SongDTO;
+import hu.flowacademy.musicstore.models.Song;
+import hu.flowacademy.musicstore.services.ArtistService;
+import hu.flowacademy.musicstore.services.SongService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/api/songs")
 public class SongController {
 
+    @Autowired
+    private SongService songService;
+
+    @GetMapping
+    List<SongDTO> findAll() {
+        List<SongDTO> listOfDtos = songService.findAll().stream().map(s -> {
+            SongDTO songDTO = new SongDTO(s);
+            return songDTO;
+        }).collect(Collectors.toList());
+        return listOfDtos;
+    }
+
+    @GetMapping("/{id}")
+    SongDTO findById(@PathVariable Long id) {
+        Song song = songService.findById(id);
+        SongDTO songDTO = new SongDTO(song);
+        return songDTO;
+    }
+
+    @PostMapping
+    SongDTO addSong(@RequestBody SongDTO songDTO) {
+        Song song = songService.addSong(songDTO);
+        SongDTO songDTO1 = new SongDTO(song);
+        return songDTO1;
+    }
+
+    @DeleteMapping("/{id}")
+    void deleteSong(@PathVariable Long id) {
+        songService.deleteSong(id);
+    }
 }
--- src/main/java/hu/flowacademy/musicstore/services/SongService.java
+++ src/main/java/hu/flowacademy/musicstore/services/SongService.java
@@ -1,7 +1,45 @@
 package hu.flowacademy.musicstore.services;
 
+import hu.flowacademy.musicstore.models.Artist;
+import hu.flowacademy.musicstore.models.DTOs.SongDTO;
+import hu.flowacademy.musicstore.models.Song;
+import hu.flowacademy.musicstore.repositories.AlbumRepository;
+import hu.flowacademy.musicstore.repositories.ArtistRepository;
+import hu.flowacademy.musicstore.repositories.SongRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Service
 public class SongService {
+
+    @Autowired
+    private SongRepository songRepository;
+
+    @Autowired
+    private AlbumService albumService;
+
+    @Autowired
+    private ArtistService artistService;
+
+    public List<Song> findAll() {
+        return songRepository.findAll();
+    }
+
+    public Song findById(Long id) {
+        return songRepository.findById(id).orElse(null);
+    }
+
+    public Song addSong(SongDTO songDTO) {
+        Song song = songDTO.toEntity();
+        song.setArtist(artistService.findById(songDTO.getArtistId()));
+        song.setAlbum(albumService.findById(songDTO.getAlbumId()));
+        return songRepository.save(song);
+    }
+
+    public void deleteSong(Long id) {
+        songRepository.deleteById(id);
+    }
 }
